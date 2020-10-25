540029b0ae6e5c8fe53ae9e1027ff5235b923afd
GerardoRosas-27 <brustergabriel24@gmail.com>
2020-10-24 22:52:42 -0500

    primer commit estructura del proyecto

---
+++ src/main/java/com/bruster/ApiRestNotasApplication.java
@@ -0,0 +1,13 @@
+package com.bruster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestNotasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestNotasApplication.class, args);
+	}
+
+}
